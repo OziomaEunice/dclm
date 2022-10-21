c460653c9e00256773538d811a51ec0715d9c7e6
OziomaEunice <onwubikooziomaeunice@gmail.com>
2022-10-21 13:09:50 +0100

    deleted folders + new .py file

---
+++ home.py
@@ -0,0 +1,18 @@
+# This is where all the programs will run
+
+# import flask | redirect user to a web page | url_for is used for creating a URL 
+# to prevent the overhead of having to change URLs 
+# throughout the application (including in templates)
+from fileinput import filename
+from os import abort
+from flask import Flask, redirect, url_for, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/home/')
+def home():
+    return render_template('Home.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
